c9286778f8cc2a5e41c51ff0be39c3b25ddc13d7
Redfluffydragon <kderu1420@gmail.com>
2019-04-10 14:56:26 -0600

    Add slider to show time in the track, and save song sources

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /**
  * Save songs
- * play other songs
+ * play other songs after current song ends
+ * Firebase?
  * 
 https://drive.google.com/open?id=1AUR-uvIKe4gDWCPweBMEWgchS3x6H9Yj
  */
@@ -21,13 +22,29 @@ let songsList = document.getElementById('songsList');
 
 let seeSong = document.getElementById('seeSong');
 
+let timeIndicate = document.getElementById('timeIndicate');
+let trackTime = document.getElementById('trackTime');
+let doneGone = document.getElementById('doneGone');
+let seeTime = document.getElementById('seeTime');
+
 let songs = [{title: 'Time to Say Goodbye (Acoustic)', artist: 'John Williams', song: 'test.mp3'}];
 let getFile;
 let playing = false;
 let upPop = false;
+let next = 'next';
+let currentSong = 0;
 
 let saveTime;
-let currentAudio = new Audio(songs[0].song);
+let currentAudio = document.createElement('audio');
+document.body.appendChild(currentAudio);
+
+const nextTrack = {
+  'next': () => {
+
+  },
+  'shuffle': () => {},
+  'random': () => {}
+}
 
 const gotchem = (item, defalt, type=localStorage) => {
   let getem = type.getItem(item);
@@ -39,10 +56,15 @@ const gotchem = (item, defalt, type=localStorage) => {
   }
 };
 
-const draw = () => {
-  // ~get songs from storage~
-
+const onStart = () => {
+  currentSong = gotchem('currentSong', 0);
+  songs = gotchem('songs', [{title: 'Time to Say Goodbye (Acoustic)', artist: 'John Williams', song: 'test.mp3'}]);
   saveTime = gotchem('saveTime', 0);
+}
+onStart();
+
+const draw = () => {
+  currentAudio.src = songs[currentSong].song;  
   songsList.innerHTML = '';
   for (i in songs) {
     let dt = document.createElement('dt');
@@ -54,7 +76,7 @@ const draw = () => {
     dt.appendChild(dd);
     songsList.appendChild(dt);
   }
-  seeSong.textContent = songs[0].title;
+  seeSong.textContent = songs[currentSong].title;
   clickList();
 };
 
@@ -64,23 +86,27 @@ function clickList() {
   for (let i = 0; i < getList.length; i++) {
     getList[i].addEventListener('click', () => {
       let tempName = getList[i].textContent.replace(removeArtist[i].textContent, '');
+      let findName = getList[i].textContent;
       let tempSong;
       for (j in songs) {
-        if (songs[j].title === tempName) {
-          tempSong = new Audio(songs[j].song);
+        if (songs[j].title+songs[j].artist === findName) {
+          tempSong = songs[j].song;
         }
       }
-      let wasPlaying = false;
-      pause();
-      if (playing) { wasPlaying = true;}
-      if (currentAudio.src !== tempSong.src) {
+      if (songs[currentSong].song !== tempSong) {
+        for (let i = 0; i < songs.length; i++) {
+          if (tempName === songs[i].title) {
+            currentSong = i;
+            localStorage.setItem('currentSong', JSON.stringify(currentSong));
+          }
+        }
         saveTime = 0;
-        currentAudio = tempSong;
-        wasPlaying = false;
-      };
-      if (!wasPlaying) { play(); }
+        currentAudio.src = songs[currentSong].song;
+        currentAudio.currentTime = saveTime;
+        play();
+      }
+      else { toggle(); }
       seeSong.textContent = tempName;
-      console.log(currentAudio.duration);
     }, false);
   }
 };
@@ -107,60 +133,108 @@ const toggle = () => {
   playing ? pause() : play();
 };
 
+function toMinutes(time) {
+  let minutes = Math.trunc(time/60);
+  let seconds = Math.round((Math.trunc(time-minutes*60)/100)*100);
+  if (seconds < 10) {
+    seconds = '0' + seconds;
+  }
+  return minutes + ':' + seconds;
+}
+
 playPause.addEventListener('click', () => {
-  // currentAudio.play();
   toggle();
   playPause.blur();
 }, false);
+
+let temptitle;
+let tempartist;
 
 uploadbtn.addEventListener('click', () => {
   let newSong;
   let title;
   let artist;
-  let temptitle;
-  if (getFile) {
-    temptitle = getFile.name.slice(0, -4);
-    newSong = getFile.name;
-  }
-  else if (byUrl.value !== '') {
+  /* if (byUrl.value !== '') {
     let url = byUrl.value;
     newSong = url.includes('drive.google') ? 'http://docs.google.com/uc?export=open&id='+url.slice(-33) : url;
     temptitle = url.slice(0, 40);
-    check = new Audio(newSong);
-    console.log(check);
-    if (!check.GET) {
-      alert('No file found');
-      return;
-    }
+  }
+  else  */if (getFile) {
+    newSong = getFile.name;
   }
   else {
-    console.log('No audio to upload.');
     return;
   }
   title = getTitle.value ? getTitle.value : temptitle;
-  artist = getArtist.value ? getArtist.value : 'unknown';
+  artist = getArtist.value ? getArtist.value : tempartist;
   playing = false;
   songs.push({title: title, artist: artist, song: newSong});
-  // ~store new songs~
+  localStorage.setItem('songs', JSON.stringify(songs));
   draw();
   closeAll();
 }, false);
 
+//when a file is picked
 file.addEventListener('change', () => {
   getFile = file.files[0];
   showFile.textContent = getFile.name;
+  let getnames = (getFile.name.slice(0, -4)).split(' - ');
+  if (getnames[1] !== undefined) {
+    temptitle = getnames[1];
+    tempartist = getnames[0];
+  }
+  else {
+    temptitle = getFile.name.slice(0, -4);
+    tempartist = 'unknown';
+  }
+  getTitle.value = temptitle;
+  getArtist.value = tempartist;
   byUrl.disabled = true;
 }, false);
 
+//open the upload modal
 uploadPopbtn.addEventListener('click', () => {
   uploadpopup.classList.add('inlineBlock');
   upPop = true;
+}, false);
+
+currentAudio.addEventListener('timeupdate', () => {
+  requestAnimationFrame(()=> {
+    let percent = 100 * (currentAudio.currentTime/currentAudio.duration);
+    doneGone.style.width = percent + '%';
+    timeIndicate.style.marginLeft = doneGone.offsetWidth + 'px';
+  });
+  seeTime.textContent = `${toMinutes(currentAudio.currentTime)}/${toMinutes(currentAudio.duration)}`
+}, false);
+
+function moveIndicator(e) {
+  e.preventDefault();
+  let newTime = e.clientX - trackTime.offsetLeft;
+  timePercent = newTime/trackTime.offsetWidth;
+  saveTime = timePercent*currentAudio.duration;
+  currentAudio.currentTime = saveTime;
+};
+
+trackTime.addEventListener('click', moveIndicator, false);
+
+timeIndicate.addEventListener('mousedown', e => {
+  document.addEventListener('mousemove', moveIndicator, false);
+}, false);
+
+document.addEventListener('mouseup', e => {
+  document.removeEventListener('mousemove', moveIndicator, false);
+}, false);
+
+currentAudio.addEventListener('onended', () => {
+  nextTrack[next]();
 }, false);
 
 const closeAll = () => {
   uploadpopup.classList.remove('inlineBlock');
   getTitle.value = '';
   getArtist.value = '';
+  temptitle = '';
+  tempartist = 'unknown';
   byUrl.value = '';
   byUrl.disabled = false;
   showFile.textContent = 'No file chosen';
@@ -181,4 +255,11 @@ document.addEventListener('mousedown', e => {
 window.addEventListener('beforeunload', () => {
   saveTime = currentAudio.currentTime;
   localStorage.setItem('saveTime', JSON.stringify(saveTime));
+}, false);
+
+document.getElementById('clearall').addEventListener('click', () => {
+  localStorage.clear();
+  sessionStorage.clear();
+  location.reload();
+  console.log('Cleared!');
 }, false);
